859043dd75720041aa0e1554de5bfa1a02c33090
Kevin Ha <kevinha298@live.com>
2020-03-20 21:14:37 -0700

    Added GetRealTimeQuote module.

---
+++ GetRealTimeQuote.py
@@ -0,0 +1,81 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import Database as db
+from datetime import datetime
+
+def parsePrice(symbol, lapse):
+    iex_api_key = 'sk_7435bdd8a15b4362b04413e189755203'
+    last_sale_price = 1000  # default last sale price
+    if lapse % 3 == 0:
+        url = f'https://finance.yahoo.com/quote/{symbol}?p={symbol}'
+        r = requests.get(url)
+        soup = bs4.BeautifulSoup(r.text, features="xml")
+        try:
+            last_sale_price = soup.find_all('div',{'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+            prev_close = soup.find_all('table', {'class': 'W(100%)'})[0].find('span', class_="Trsdu(0.3s) ").text
+            price_and_percent_change = str(soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find_all('span')[1].text)
+            percent_change = price_and_percent_change[price_and_percent_change.find('(') + 1:-2]
+            percent_change = float(percent_change.replace('+', ''))
+            day_range = soup.find_all('div', {'class': 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($c-fuji-grey-c)'})[0].find_all('td')[9].text
+            day_high = str(day_range)[0:day_range.find('-') - 1]
+            day_low = str(day_range)[day_range.find('-') + 2:]
+        except Exception as error:
+            db_conn = db.connect()
+            db.insert_run_log(db_conn, str(datetime.now()), f'Encountered an exception when trying to retrieve price for {symbol} from Yahoo.', error, soup)
+        print(f'Yahoo: symbol = {symbol},  price = {last_sale_price}')
+    elif lapse % 3 == 1:
+        url = f'https://www.nasdaq.com/symbol/{symbol}/real-time'
+        url_quote = f'https://www.nasdaq.com/symbol/{symbol}'
+        r = requests.get(url)
+        r_quote = requests.get(url_quote)
+        soup = bs4.BeautifulSoup(r.text, features="lxml")
+        soup_quote = bs4.BeautifulSoup(r_quote.text, features="lxml")
+        last_sale_price = ''
+        try:
+            last_sale_price = soup.find_all('div', {'class': 'qwidget-dollar', 'id': 'qwidget_lastsale'})[0].text
+            last_sale_price = float(last_sale_price[1:])
+            percent_change = soup.find_all('div', {'class': 'qwidget-percent', 'id': 'qwidget_percent'})[0].text
+            percent_change = float(percent_change[:-1])
+            day_high_low = soup_quote.find_all('div', {'class': 'table-table fontS14px'})[0].find_all('div', {'class': 'table-row'})[1].find_all('div', {'class': 'table-cell'})[1].text
+            day_high_low = str(day_high_low)[day_high_low.find('$'):]
+            day_high = float(day_high_low[day_high_low.find('$') + 2:day_high_low.find('/') - 1])
+            day_low = float(day_high_low[day_high_low.find('/') + 3:day_high_low.find('/') + 15])
+            prev_close = soup_quote.find_all('div', {'class': 'table-table fontS14px'})[0].find_all('div', {'class': 'table-row'})[4].find_all('div', {'class': 'table-cell'})[1].text
+            prev_close = str(prev_close)[prev_close.find('$'):]
+            prev_close = float(prev_close[prev_close.find('$') + 2:prev_close.find('.') + 5])
+            if last_sale_price < prev_close:
+                percent_change *= -1
+        except Exception as error:
+            db_conn = db.connect()
+            db.insert_run_log(db_conn, str(datetime.now()),f'Encountered an exception when trying to retrieve price for {symbol} from Nasdaq.',error, soup)
+        print(f'Nasdaq: symbol = {symbol},  price = {last_sale_price}')
+    elif lapse % 3 == 2:
+        url = f'https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={iex_api_key}'
+        try:
+            r = requests.get(url)
+            r_json = r.json()
+            last_sale_price = r_json['latestPrice']
+            percent_change = float(r_json['changePercent'] * 100)
+            day_low = r_json['low']
+        except Exception as error:
+            db_conn = db.connect()
+            db.insert_run_log(db_conn, str(datetime.now()), f'Encountered an exception when trying to retrieve price for {symbol} from IEX.', error, url)
+        print(f'IEX: symbol = {symbol},  price = {last_sale_price}')
+
+        # url = f'https://cloud.iexapis.com/stable/tops?token={iex_api_key}&symbols={symbol}'
+        # try:
+        #     r = requests.get(url)
+        #     r_json = r.json()
+        #     r_list = r_json[0]
+        #     last_sale_price = r_list['lastSalePrice']
+        # except Exception as error:
+        #     db_conn = db.connect()
+        #     db.insert_run_log(db_conn, str(datetime.now()), f'Encountered an exception when trying to retrieve price for {symbol} from IEX.', error, url)
+        # print(f'IEX: {last_sale_price}')
+    return float(last_sale_price), float(percent_change), float(day_low)
+
+
+
+
--- Scalp.py
+++ Scalp.py
@@ -33,5 +33,94 @@ to_zone = tz.gettz('America/New_York')
 
 db_conn = db.connect()
 
-db.insert_run_log(db_conn, str(datetime.now()), 'Test database connection.', '', '')
+def app_run_cond():
+    app_config = db.execute_query(db_conn, "SELECT * FROM app_config WHERE config_name = 'is_app_active'")
+    run_app = False
+    clock = api.get_clock()
+    for config in app_config:
+        run_app = True if config[0] == 'is_app_active' and int(config[1]) == 1 and datetime.today().weekday() < 6 and clock.is_open else False
+    return run_app
+
+lapse = 0
+while app_run_cond():
+    lapse += 1
+    print(f'Run #: {lapse}')
+
+    for j in range(1,3):
+        # Get a list of filled orders.  To improve speed we want to only get recently filled orders today.
+        orders = [o for o in api.list_orders(status='closed') if o.status == 'filled']
+        insert_values = ''
+        for order in orders:
+            order = str(order)[str(order).find('(')+1:-1]
+            order = order.replace("Order({   '","{'")
+            order = literal_eval(order)
+            filled_at = str(order['filled_at'])[:10] +' ' + str(order['filled_at'])[11:19]
+            filled_at = datetime.strptime(filled_at, '%Y-%m-%d %H:%M:%S')
+            filled_at = filled_at.replace(tzinfo=from_zone)
+            filled_at_east_std_time = str(filled_at.astimezone(to_zone))[:-6]
+            order_values = ''
+            for keys,values in order.items():
+                order_values += f"'{values}',"
+            order_values = f"{order_values}'{filled_at_east_std_time}'"
+            insert_values += f"({order_values}),"
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Retrieved all recent orders data from API.', NULL, NULL)
+        print(str(datetime.now()) + ': Retrieved all recent orders data from API.')
+
+        # update filled orders into back-end database.
+        insert_values = insert_values[:-1]
+        db.truncate(db_conn, 'filled_order_stage')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Truncated stage table in database.', NULL, NULL)
+        print(str(datetime.now()) + ': Truncated stage table in database.')
+
+        db.insert_filled_order(db_conn, insert_values)
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated stage table in database with recent order data from API.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated stage table in database with recent order data from API.')
+
+        db.call_store_procedure(db_conn, 'add_new_filled_order')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated recent order data from stage table to order table.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated recent order data from stage table to order table.')
+
+
+        db.call_store_procedure(db_conn, 'add_transaction_buy')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated recent order into buy transaction table.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated recent order into buy transaction table.')
+
+        db.call_store_procedure(db_conn, 'add_transaction_sell')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated recent order into sell transaction table.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated recent order into sell transaction table.')
+
+        db.call_store_procedure(db_conn, 'update_transaction')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Calculated and synchronized buy and sell transactions and updated open positions.', NULL, NULL)
+        print(str(datetime.now()) + ': Calculated and synchronized buy and sell transactions and updated open positions.')
+
+        db.call_store_procedure(db_conn, 'update_queue_sell')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated database queue for future sell orders to submit to API.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated database queue for future sell orders to submit to API.')
+
+        db.call_store_procedure(db_conn, 'update_queue_buy')
+        # db.insert_run_log(db_conn, str(datetime.now()), 'Updated database queue for future buy orders to submit to API.', NULL, NULL)
+        print(str(datetime.now()) + ': Updated database queue for future buy orders to submit to API.')
+
+        time.sleep(3)
+
+    sell_order = db.order_rule(db_conn,'sell')
+    buy_order = db.order_rule(db_conn,'buy')
+    # db.insert_run_log(db_conn, str(datetime.now()), 'Retrieved rules for buy and sell orders from database.', NULL, NULL)
+    print(str(datetime.now()) + ': Retrieved rules for buy and sell orders from database.')
+
+    open_orders = api.list_orders(status='open')
+    open_positions = api.list_positions()
+    account = api.get_account()
+    order_type = 'limit'
+    # db.insert_run_log(db_conn, str(datetime.now()), 'Retrieved current open orders and open positions from API.', NULL, NULL)
+    print(str(datetime.now()) + ': Retrieved current open orders and open positions from API.')
+
+    so.sellOrder(sell_order, account, open_orders, open_positions, lapse, api, order_type, db_conn)
+
+    bo.buyOrder(buy_order, account, open_orders, open_positions, lapse, api, order_type, db_conn)
+
+db_conn.close()
+
+
+
 
